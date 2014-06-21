66677f00730fcf9ae280936801fd643cd914b9f4
Samantha Yeargin <slyeargin@gmail.com>
2014-06-21 14:22:05 -0500

    DIFF

--- app/static/js/es6/main.es6.js
+++ app/static/js/es6/main.es6.js
@@ -1,10 +1,24 @@
-// (function() {
-//   'use strict';
-//
-//   $(document).ready(init);
-//
-//   function init(){
-//     $('.dropdown-toggle').dropdown();
-//   }
-//
-// })();
+(function() {
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    $('.dropdown-toggle').dropdown();
+    $('.panel').click(getDiff);
+  }
+
+  function getDiff(){
+    var current = $(this).data('id');
+    var previous = current + 1;
+
+    $(this).prettyTextDiff({
+      originalContainer: '#copy' + previous,
+      changedContainer: '#copy' + current,
+      diffContainer: '#diff' + current,
+      cleanup: true,
+      debug: true
+    });
+  }
+
+})();
