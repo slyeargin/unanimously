2022db7ac3edeac7888be743b4d70c3bb7e98c23
Samantha Yeargin <slyeargin@gmail.com>
2014-12-08 00:23:15 -0600

    Remove sockets.io from project.

--- app/app.js
+++ app/app.js
@@ -34,9 +34,4 @@ server.listen(port, function(){
   console.log('Node server listening. Port: ' + port + ', Database: ' + dbname);
 });
 
-/* --- socket.io        */
-var sockets = traceur.require(__dirname + '/lib/sockets.js');
-var io = require('socket.io')(server);
-io.of('/app').on('connection', sockets.connection);
-
 module.exports = app;
